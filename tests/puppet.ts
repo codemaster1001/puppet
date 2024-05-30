dafcd1db501a536b89b6e8871b6ac3a6e8714055
codemaster1001 <blockchaincrazy1001@gmail.com>
2024-05-30 09:53:43 +0200

    cpi complete

---
+++ tests/puppet.ts
@@ -0,0 +1,45 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Keypair } from "@solana/web3.js";
+import { expect } from "chai";
+import { Puppet } from "../target/types/puppet";
+import { PuppetMaster } from "../target/types/puppet_master";
+
+describe("puppet", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const puppetProgram = anchor.workspace.Puppet as Program<Puppet>;
+  const puppetMasterProgram = anchor.workspace
+    .PuppetMaster as Program<PuppetMaster>;
+
+  const puppetKeypair = Keypair.generate();
+  const authorityKeypair = Keypair.generate();
+
+  it("Does CPI!", async () => {
+    await puppetProgram.methods
+      .initialize(authorityKeypair.publicKey)
+      .accounts({
+        puppet: puppetKeypair.publicKey,
+        user: provider.wallet.publicKey,
+      })
+      .signers([puppetKeypair])
+      .rpc();
+
+    await puppetMasterProgram.methods
+      .pullStrings(new anchor.BN(42))
+      .accounts({
+        puppet: puppetKeypair.publicKey,
+        authority: authorityKeypair.publicKey,
+        // puppetProgram: puppetProgram.programId,
+      })
+      .signers([authorityKeypair])
+      .rpc();
+
+    expect(
+      (
+        await puppetProgram.account.data.fetch(puppetKeypair.publicKey)
+      ).data.toNumber()
+    ).to.equal(42);
+  });
+});
